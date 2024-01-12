28a90ae5b243f03389740b0f24dae72da57c4a06
Hojoon Kim <hojoon0724@gmail.com>
2024-01-11 22:12:25 -0800

    Index page CSS done.

--- models/seed.js
+++ models/seed.js
@@ -49,4 +49,52 @@ module.exports = [
     desktop_img: "https://www.visitdubai.com/-/media/gathercontent/poi/b/burj-khalifa/fallback-image/poi-burj-khalifa-3-dtcm-jun-2023.jpg",
     mobile_img: "https://www.visitdubai.com/-/media/gathercontent/poi/b/burj-khalifa/fallback-image/poi-burj-khalifa-3-dtcm-jun-2023.jpg",
   },
+  {
+    city: "Berlin",
+    country: "Germany",
+    desktop_img: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4b/Museumsinsel_Berlin_Juli_2021_1_%28cropped%29.jpg/1200px-Museumsinsel_Berlin_Juli_2021_1_%28cropped%29.jpg",
+    mobile_img: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4b/Museumsinsel_Berlin_Juli_2021_1_%28cropped%29.jpg/1200px-Museumsinsel_Berlin_Juli_2021_1_%28cropped%29.jpg",
+  },
+  {
+    city: "Rome",
+    country: "Italy",
+    desktop_img: "https://www.italyperfect.com/g/photos/upload/sml_845543004-1590582528-ip-info-rome.jpg",
+    mobile_img: "https://www.italyperfect.com/g/photos/upload/sml_845543004-1590582528-ip-info-rome.jpg",
+  },
+  {
+    city: "Beijing",
+    country: "China",
+    desktop_img: "https://travel2next.com/wp-content/uploads/beijing-night.jpg",
+    mobile_img: "https://travel2next.com/wp-content/uploads/beijing-night.jpg",
+  },
+  {
+    city: "Mumbai",
+    country: "India",
+    desktop_img: "https://images.contentstack.io/v3/assets/blt00454ccee8f8fe6b/blt3f1dd78cb088d203/61d6b8af0d4d1813c7f45eb5/US_Mumbai_IN_Header.jpg",
+    mobile_img: "https://images.contentstack.io/v3/assets/blt00454ccee8f8fe6b/blt3f1dd78cb088d203/61d6b8af0d4d1813c7f45eb5/US_Mumbai_IN_Header.jpg",
+  },
+  {
+    city: "Moscow",
+    country: "Russia",
+    desktop_img: "https://www.nationsonline.org/gallery/Russia/State-Historical-Museum-Moscow.jpg",
+    mobile_img: "https://www.nationsonline.org/gallery/Russia/State-Historical-Museum-Moscow.jpg",
+  },
+  {
+    city: "Mexico City",
+    country: "Mexico",
+    desktop_img: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/Mexico_City_Reforma_Street.jpg/800px-Mexico_City_Reforma_Street.jpg",
+    mobile_img: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/Mexico_City_Reforma_Street.jpg/800px-Mexico_City_Reforma_Street.jpg",
+  },
+  {
+    city: "Bangkok",
+    country: "Thailand",
+    desktop_img: "https://content.r9cdn.net/rimg/dimg/26/5b/01e97574-city-26166-1592813274a.jpg?width=1366&height=768&xhint=1038&yhint=725&crop=true",
+    mobile_img: "https://content.r9cdn.net/rimg/dimg/26/5b/01e97574-city-26166-1592813274a.jpg?width=1366&height=768&xhint=1038&yhint=725&crop=true",
+  },
+  {
+    city: "Istanbul",
+    country: "Turkey",
+    desktop_img: "https://a.cdn-hotels.com/gdcs/production6/d781/3bae040b-2afb-4b11-9542-859eeb8ebaf1.jpg",
+    mobile_img: "https://a.cdn-hotels.com/gdcs/production6/d781/3bae040b-2afb-4b11-9542-859eeb8ebaf1.jpg",
+  },
 ];
